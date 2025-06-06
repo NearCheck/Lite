409613c2a8511f6dd11b1400d1eb91e3d75c57b8
NearCheck <nearchecklite@gmail.com>
2025-06-06 14:17:19 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+  firebase: {
+          apiKey: "AIzaSyDFBb1l7GHsPbCMT9_XI6Lqc88dxaAydTQ",
+          authDomain: "lite-50af2.firebaseapp.com",
+          databaseURL: "https://lite-50af2-default-rtdb.firebaseio.com",
+          projectId: "lite-50af2",
+          storageBucket: "lite-50af2.firebasestorage.app",
+          messagingSenderId: "259126907909",
+          appId: "1:259126907909:web:2026713fbcd6cd4442f86c",
+          measurementId: "G-9DSQJ1PMHY"
+            },
+          ipGeolocationApi: "https://ipapi.co/json/",
+          mapTileServer: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          mapAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            };
